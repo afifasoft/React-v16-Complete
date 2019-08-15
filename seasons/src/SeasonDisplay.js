8a133fdf826cc0e36f8b191b01bb1e020a7f5026
afifasoft <abdul.pesit@gmail.com>
2019-08-16 01:20:02 +0400

    life cycle methods - React

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -1,8 +1,27 @@
 import React from 'react';
 
-const SeasonDisplay = () => {
+
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? 'summer' : 'winter';
+  } else {
+    return lat > 0 ? 'winter' : 'summer';
+  }
+}
+
+const SeasonDisplay = (props) => {
+  console.log('inside SeasonDisplay');
+  console.log(props.lat);
+  const season = getSeason(props.lat, new Date().getMonth());
+  console.log(season);
+  const text = season === 'winter' ? 'Burr, it is chilly' : 'Lets hit the beach!';
+  const icon = season === 'winter' ? 'snowflake' : 'sun';
   return (
-    <div>Season Display</div>
+    <div>
+      <i className={`${icon} icon`} />
+      <h1>{text}</h1>
+      <i className={`${icon} icon`} />
+    </div>
   );
 };
 
--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,30 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
 
-    this.state = {
-      lat: null,
-      errorMessage: ''
-    };
+  // constructor(props) {
+  //   super(props);
+  //
+  //   this.state = {
+  //     lat: null,
+  //     errorMessage: ''
+  //   };
+  //
+  // }
 
+  state = {lat: null, errorMessage: ''};
+  // Only one time this life cycle method is called
+  componentDidMount() {
+    console.log('My component was rendered to the screen');
     window.navigator.geolocation.getCurrentPosition(
       (position) => {
         console.log(position)
-        this.setState({lat: position.coords.latitude})
+        this.setState({lat: position.coords.latitude});
+
       },
       (err) => {
-        this.setState({
-          errorMessage: err.message
-        });
         console.log(err)
+        this.setState({ errorMessage: err.message });
+
       }
     );
 
+}
+
+  // this method is called when the setState() is called
+  componentDidUpdate() {
+    console.log('My componet was just updated -it rerendered!');
   }
+
 
 // React says we have to define render!!
   render () {
@@ -33,7 +46,9 @@ class App extends React.Component {
     }
 
     if (!this.state.errorMessage && this.state.lat) {
-      return <div>Latitude: {this.state.lat}</div>;
+
+      return <SeasonDisplay lat={this.state.lat}/>
+    //  return <div>Latitude: {this.state.lat}</div>;
     }
     return <div>Loading!</div>
   }
