5ce9bd4fb8691fbdb610d7c0363dcf5647e8dd1e
afifasoft <abdul.pesit@gmail.com>
2019-08-28 00:04:05 +0400

    props in react

---
+++ pics/src/Playgrounds.js
@@ -0,0 +1,23 @@
+class Car {
+
+  constructor() {
+  	this.drive = this.drive.bind(this);
+  }
+
+  setDriveSound(sound) {
+  	this.sound= sound;
+  }
+
+  drive() {
+  	return this.sound;
+  }
+
+
+}
+
+
+const car = new Car();
+car.setDriveSound('vroom');
+
+const drive = car.drive;
+drive()
--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -3,26 +3,40 @@ import React from 'react';
 class SearchBar extends React.Component {
 
 
-  onInputChange(event) {
-    event.preventDefault();
-    console.log(event.target.value)
-    console.log('search text');
+  state = {term: ''};
+
+  constructor() {
+    super();
+    this.onFormSubmit = this.onFormSubmit.bind(this);
+
   }
 
-  onInputClick(event) {
+  // onInputChange(event) {
+  //   event.preventDefault();
+  //   console.log(event.target.value)
+  //   console.log('search text');
+  // }
+
+  // onInputClick(event) {
+  //   event.preventDefault();
+  //   console.log('clicked');
+  //   //  <input type="submit" onClick={this.onInputClick} />
+  // }
+
+  onFormSubmit(event) {
     event.preventDefault();
-    console.log('clicked');
-    //  <input type="submit" onClick={this.onInputClick} />
+    this.props.onSubmit(this.state.term)   // when class based componet we will use this.props and onSubmit is props refernce from App.js
+                          // when function based component we will use props
+  //  console.log(this.state.term);
   }
 
   render() {
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form  onSubmit={(event) => this.onFormSubmit(event)} className="ui form">
           <div className="field">
             <label>Image Search</label>
-            <input type="text" onChange={this.onInputChange}/>
-
+            <input type="text" value={this.state.term}  onChange={e => this.setState({ term: e.target.value})}/>
           </div>
         </form>
       </div>
