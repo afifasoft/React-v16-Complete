65faa7e56e157ab05673ec078215821f139dd56a
afifasoft <abdul.pesit@gmail.com>
2019-12-02 11:13:23 +0400

    Redux and React-Redux

---
+++ Redux.js
@@ -0,0 +1,35 @@
+// 135
+
+console.clear();
+// People dropping of a form (Action creator)
+
+const createPolicy = (name, amount) => {
+  return {  // Action ( a form in our  analogy )
+    type: 'CREATE_POLICY',
+    payload: {
+      name: name,
+      amount: amount
+    }
+  };
+};
+
+
+const deletePolicy = (name) => {
+  return {
+    type: 'DELETE_POLICY',
+     payload: {
+       name: name
+     }
+  };
+};
+
+
+const createClaim = (name, amountOfMoneyToCollect) => {
+  return {
+    type: 'CREATE_CLAIM',
+    payload: {
+      name: name,
+      amountOfMoneyToCollect: amountOfMoneyToCollect
+    }
+  };
+};
