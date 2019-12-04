fc872558e8e5b0c0d453a80367fa7c712a214688
afifasoft <abdul.pesit@gmail.com>
2019-12-04 08:10:54 +0400

    blog initial commit

--- songs/src/components/SongDetail.js
+++ songs/src/components/SongDetail.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-const SongDetail = (props) => {
+//const SongDetail = (props) and below we destructred
+const SongDetail = ({ song }) => {
 
-  console.log(props);
-  return <div>Song Detail</div>
+  console.log(song);
+  if (!song) {
+      return <div>Select a song</div>
+  }
+  return (
+      <div>
+        <h3>Details for: </h3>
+        <p>
+          Title: {song.title}
+          <br />
+          Duration: {song.duration}
+        </p>
+
+      </div>
+  );
 
 };
 
