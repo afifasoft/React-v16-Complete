32ca7ca2fd5ed1ad0ada86d3a640873a55474d74
afifasoft <abdul.pesit@gmail.com>
2019-12-05 08:27:38 +0400

    api request

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,5 +1,13 @@
-export const fetchPosts = () => {
+import jsonPlaceholder  from '../apis/jsonPlaceholder';
+
+
+export const fetchPosts = async () => {
+
+  const response = await jsonPlaceholder.get('/posts');
+
   return {
-    type: 'FETCH_POSTS'
+    type: 'FETCH_POSTS',
+    payload: response
   };
+  
 };
