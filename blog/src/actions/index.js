1c78d0397cc78dac055612b63207e1e5b8dc836f
afifasoft <abdul.pesit@gmail.com>
2019-12-08 00:43:50 +0400

    redux-thunk

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,13 +1,18 @@
 import jsonPlaceholder  from '../apis/jsonPlaceholder';
 
+// function return a function
+export const fetchPosts = () => async dispatch => {
 
-export const fetchPosts = async () => {
+    const response =  await jsonPlaceholder.get('/posts');
 
-  const response = await jsonPlaceholder.get('/posts');
+    dispatch({type: 'FETCH_POSTS', payload: response})
 
-  return {
-    type: 'FETCH_POSTS',
-    payload: response
-  };
-  
 };
+
+// export const fetchPosts = () => {
+// return async function(dispatch, getState) {
+//  const response = await jsonPlaceholder.get('/posts');
+//  dispatch({ type: 'FETCH_POSTS', payload: response });
+// }
+
+// };
--- blog/src/reducers/index.js
+++ blog/src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
-
+import postsReducers from './postsReducers';
 
 export default combineReducers({
-  replaceMe: () => 'hi there'
+
+  posts: postsReducers
+
 });
