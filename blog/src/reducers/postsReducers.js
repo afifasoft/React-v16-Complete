7980a9cc73735bdb3404c83a32cb958614923d84
afifasoft <abdul.pesit@gmail.com>
2019-12-22 22:58:45 +0400

    PostReducer created

--- blog/src/reducers/postsReducers.js
+++ blog/src/reducers/postsReducers.js
@@ -1,3 +1,15 @@
-export default () => {
-  return 123;
-}
+export default (state, action) => {
+  return 123; 
+};
+
+
+
+
+// bad !
+// return document.querySelector('input')
+
+// bad !
+// return axios.get('/posts')
+
+// good
+// return state + action
