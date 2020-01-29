f3869100ddfdd20f39cb9c377a9517e4d76ee1a6
afifasoft <abdul.pesit@gmail.com>
2020-01-29 08:45:58 +0530

    Updating Auth State

--- streams/client/src/components/GoogleAuth.js
+++ streams/client/src/components/GoogleAuth.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 class GoogleAuth extends Component {
-
+  state = { isSignedIn: null };
   componentDidMount() {
 
     window.gapi.load('client:auth2', () => {
@@ -10,14 +10,34 @@ class GoogleAuth extends Component {
         clientId: '564012691152-mfadgrurg8c9dlr6bnmgknqbjnnn0dmr.apps.googleusercontent.com',
         scope : 'email'
 
+      }).then(() => {
+        this.auth = window.gapi.auth2.getAuthInstance();
+        this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+        this.auth.isSignedIn.listen(this.onAuthChange);
       });
     });
 
   }
 
+  onAuthChange = () => {
+
+    this.setState({ isSignedIn : this.auth.isSignedIn.get() });
+
+  };
+
+  renderAuthButton() {
+    if (this.state.isSignedIn === null) {
+      return <div>I dont know if we are signed in</div>
+    } else if (this.state.isSignedIn) {
+      return <div>I am signed in!</div>
+    } else {
+      return <div>I am not signed in</div>
+    }
+  }
+
   render() {
     return (
-      <div>Google Auth</div>
+      <div>{this.renderAuthButton()}</div>
     );
   }
 };
--- streams/client/src/index.js
+++ streams/client/src/index.js
@@ -4,7 +4,6 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 
 
-// 212
 
 ReactDOM.render(
   <App/>,
