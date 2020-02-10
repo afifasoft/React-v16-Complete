c958ed2d2317d0b40673ef79edb17e2b9ee67ff0
afifasoft <abdul.pesit@gmail.com>
2020-02-10 08:57:58 +0530

    Merging Lists of Records

--- streams/client/src/reducers/streamReducer.js
+++ streams/client/src/reducers/streamReducer.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import {
   CREATE_STREAM,
   FETCH_STREAMS,
@@ -10,14 +11,19 @@ import {
 
 export default (state = {}, action) => {
   switch (action.type) {
+    case FETCH_STREAMS:
+      return { ...state, ..._.mapKeys(action.payload, 'id') };
+
     case FETCH_STREAM:
-      return { ...state, [action.payload.id]: action.payload }; // returns single record 
+      return { ...state, [action.payload.id]: action.payload }; // returns single record
 
     case CREATE_STREAM:
       return { ...state, [action.payload.id]: action.payload };
 
     case EDIT_STREAM:
       return { ...state, [action.payload.id]: action.payload };
+    case DELETE_STREAM:
+        return _.omit(state, action.payload);
     default:
       return state;
   }
