81b0f4fbc67633ebd657a326f80b07370a6a2480
afifasoft <abdul.pesit@gmail.com>
2020-02-13 08:40:28 +0530

    Implementing Programmatic Navigation

--- streams/client/src/actions/index.js
+++ streams/client/src/actions/index.js
@@ -1,4 +1,5 @@
-import streams from '../apis/streams'
+import streams from '../apis/streams';
+import history from '../history';
 import {
   SIGN_IN,
   SIGN_OUT,
@@ -31,9 +32,7 @@ export const createStream = formValues =>  async (dispatch, getState) => {
   const { userId } = getState().auth;
   const response = await streams.post('/streams', { ...formValues, userId });
   dispatch({ type: CREATE_STREAM, payload: response.data });
-
-  // Do some programmatic navigation to
-  // get the user back to the root route
+  history.push('/');
 };
 
 export const fetchStreams = () => async (dispatch) => {
--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { Router, Route } from 'react-router-dom';
 import StreamCreate from './streams/StreamCreate';
 import StreamEdit from './streams/StreamEdit';
 import StreamDelete from './streams/StreamDelete';
@@ -7,13 +7,14 @@ import StreamList from './streams/StreamList';
 import StreamShow from './streams/StreamShow';
 import Header from './Header';
 import 'semantic-ui-css/semantic.min.css';
+import history from '../history';
 
 
 const App = () =>  {
   return (
     <div className="ui container">
 
-      <BrowserRouter>
+      <Router history={history}>
         <div>
           <Header/>
           <Route path="/" exact component={StreamList} />
@@ -22,7 +23,7 @@ const App = () =>  {
           <Route path="/streams/delete" component={StreamDelete} />
           <Route path="/streams/show" component={StreamShow} />
         </div>
-      </BrowserRouter>
+      </Router>
     </div>
 
   );
