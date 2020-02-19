936c6b0dbe6fff231fe7f961212e406cb086ce03
afifasoft <abdul.pesit@gmail.com>
2020-02-19 08:38:49 +0530

    React Fragments

--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -20,7 +20,7 @@ const App = () =>  {
           <Route path="/" exact component={StreamList} />
           <Route path="/streams/new" component={StreamCreate} />
           <Route path="/streams/edit/:id" component={StreamEdit} />
-          <Route path="/streams/delete/:id" component={StreamDelete} />
+          <Route path="/streams/delete" component={StreamDelete} />
           <Route path="/streams/show/:id" component={StreamShow} />
         </div>
       </Router>
--- streams/client/src/components/streams/StreamDelete.js
+++ streams/client/src/components/streams/StreamDelete.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import Modal from '../Modal';
+import 'semantic-ui-css/semantic.min.css';
 
 const StreamDelete = () => {
-  return <div>StreamDelete</div>;
+
+  const actions = (
+    <React.Fragment>
+      <button className="ui button negative">Delete</button>
+      <button className="ui button">Cancel</button>
+    </React.Fragment>
+  );
+
+  return (
+    <div>
+      StreamDelete
+      <Modal
+        title="Delete Stream"
+        content="Are you sure you want to delete this stream ?"
+        actions={actions}
+        />
+    </div>
+  );
 };
 
 export default StreamDelete;
