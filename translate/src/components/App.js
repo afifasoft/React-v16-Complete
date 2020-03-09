0244ab0c6b042235c18cc0ae0ff85fd6d26e88be
afifasoft <abdul.pesit@gmail.com>
2020-03-09 08:55:52 +0530

    Gotchas Around Providers

--- translate/src/components/App.js
+++ translate/src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import 'semantic-ui-css/semantic.min.css';
 import UserCreate from './UserCreate';
+import LanguageContext from '../contexts/LanguageContext';
 
 class App extends Component {
 
@@ -18,9 +19,14 @@ class App extends Component {
           <i className="flag us" onClick={() => this.onLanguageChange('english')}/>
           <i className="flag ae" onClick={() => this.onLanguageChange('dutch')}/>
         </div>
-
+        <LanguageContext.Provider value={this.state.language}>
+          <UserCreate />
+        </LanguageContext.Provider>
+        <LanguageContext.Provider value="dutch">
+          <UserCreate />
+        </LanguageContext.Provider>
         <UserCreate />
-      </div>
+    </div>
     );
   }
 }
