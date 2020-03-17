fbe937cae1c29294b6c1703a60d7b022b901b318
afifasoft <abdul.pesit@gmail.com>
2020-03-17 07:24:04 +0530

    Code Reuse with Hooks

--- hooks-simple/src/components/ResourceList.js
+++ hooks-simple/src/components/ResourceList.js
@@ -1,29 +1,9 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React from 'react';
+import useResources  from './useResources';
 
 const ResourceList = ({ resource }) => {
-   const [resources, setResources] = useState([]);
 
-  //  const fetchResource = async (resource) =>  {
-  //
-  //   const response = await axios.get(`https://jsonplaceholder.typicode.com/${resource}`)
-  //
-  //   setResources(response.data);
-  //
-  // };
-
-  useEffect(() => {
-  //  fetchResource(resource);
-    (async (resource) =>  {
-
-     const response = await axios.get(`https://jsonplaceholder.typicode.com/${resource}`)
-
-     setResources(response.data);
-
-   })(resource);
-  }, [resource])
-
-
+  const resources = useResources(resource);
     return (
       <ul>{resources.map(record => <li key={record.id}>{record.title}</li>)}</ul>
     );
---
+++ hooks-simple/src/components/useResources.js
@@ -0,0 +1,29 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const useResources = (resource) => {
+  const [resources, setResources] = useState([]);
+
+ //  const fetchResource = async (resource) =>  {
+ //
+ //   const response = await axios.get(`https://jsonplaceholder.typicode.com/${resource}`)
+ //
+ //   setResources(response.data);
+ //
+ // };
+
+ useEffect(() => {
+ //  fetchResource(resource);
+   (async (resource) =>  {
+
+    const response = await axios.get(`https://jsonplaceholder.typicode.com/${resource}`)
+
+    setResources(response.data);
+
+  })(resource);
+ }, [resource])
+
+ return resources;
+};
+
+export default useResources;
