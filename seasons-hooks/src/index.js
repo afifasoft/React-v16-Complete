c5ec0d202837a997b942fdd0d650a5afe0f27648
afifasoft <abdul.pesit@gmail.com>
2020-03-17 08:47:57 +0530

    Refactoring a Class to a Function

---
+++ seasons-hooks/src/index.js
@@ -0,0 +1,92 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
+import Clock from './Clock';
+import useLocation from './useLocation';
+
+const App = () => {
+  
+  const [lat, errorMessage] = useLocation();
+  let content;
+
+  if (errorMessage) {
+    content = <div>{errorMessage}</div>;
+  } else if (lat) {
+    content = <SeasonDisplay lat={lat}/>
+  } else {
+    content = <Spinner message="Please accept location request"/>;
+  }
+
+  return <div className="border red">{content}</div>
+};
+
+// class App extends React.Component {
+//
+//   // constructor(props) {
+//   //   super(props);
+//   //
+//   //   this.state = {
+//   //     lat: null,
+//   //     errorMessage: ''
+//   //   };
+//   //
+//   // }
+//
+//   state = {lat: null, errorMessage: ''};
+//   // Only one time this life cycle method is called
+//   componentDidMount() {
+//     console.log('My component was rendered to the screen');
+//     window.navigator.geolocation.getCurrentPosition(
+//       (position) => {
+//         console.log(position)
+//         this.setState({lat: position.coords.latitude});
+//
+//       },
+//       (err) => {
+//         console.log(err)
+//         this.setState({ errorMessage: err.message });
+//
+//       }
+//     );
+//
+// }
+//
+//   // this method is called when the setState() is called
+//   componentDidUpdate() {
+//     console.log('My componet was just updated -it rerendered!');
+//   }
+//
+//
+//   renderContent() {
+//     if (this.state.errorMessage && !this.state.lat) {
+//       return <div>Error: {this.state.errorMessage}</div>;
+//     }
+//
+//     if (!this.state.errorMessage && this.state.lat) {
+//
+//       return (
+//         <div>
+//           <Clock/>
+//           <SeasonDisplay lat={this.state.lat}/>
+//         </div>
+//       );
+//     //  return <div>Latitude: {this.state.lat}</div>;
+//     }
+//     return <Spinner message="Please accept location request"/>;
+//   }
+//
+// // React says we have to define render!!
+//   render () {
+//     return (
+//       <div className="border red">
+//         {this.renderContent()}
+//       </div>
+//     );
+//   }
+// }
+
+ReactDOM.render(
+  <App/>,
+  document.querySelector('#root')
+);
